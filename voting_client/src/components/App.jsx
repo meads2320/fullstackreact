d9db19eae4fbe0b75c6e230cf39375063463ca62
HCI01\meads <meads@hcpci.com>
2016-08-17 16:50:37 -0400

    finished

--- voting_client/src/components/App.jsx
+++ voting_client/src/components/App.jsx
@@ -1,14 +1,7 @@
 import React from 'react';
-import {List, Map} from 'immutable';
-
-const pair = List.of('South Florida', 'Houston');
-const tally = Map({'South Florida': 5, 'Houston': 4});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {
-      pair: pair,
-      tally: tally
-    });
+    return this.props.children;
   }
 });
