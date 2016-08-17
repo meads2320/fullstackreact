1d648d934a35ddc53d24939bf6e3143ed94a4f08
HCI01\meads <meads@hcpci.com>
2016-08-17 15:57:02 -0400

    reducer

--- voting_client/src/components/App.jsx
+++ voting_client/src/components/App.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
-import {List} from 'immutable';
+import {List, Map} from 'immutable';
 
 const pair = List.of('South Florida', 'Houston');
+const tally = Map({'South Florida': 5, 'Houston': 4});
 
 export default React.createClass({
   render: function() {
-    return React.cloneElement(this.props.children, {pair: pair});
+    return React.cloneElement(this.props.children, {
+      pair: pair,
+      tally: tally
+    });
   }
 });
--- voting_client/src/index.jsx
+++ voting_client/src/index.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Route} from 'react-router';
+import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import reducer from './reducer';
 import App from './components/App';
 import Voting from './components/Voting';
+import Results from './components/Results';
+import {Provider} from 'react-redux';
 
-const pair = ['South Florida', 'Houston'];
+const store = createStore(reducer);
+store.dispatch({
+  type: 'SET_STATE',
+  state: {
+    vote: {
+      pair: ['South Florida', 'Houston'],
+      tally: {'South Florida' : 2}
+    }
+  }
+});
+
 const routes = <Route component={App}>
+  <Route path="/results" component={Results} />
   <Route path="/" component={Voting} />
 </Route>;
 
 ReactDOM.render(
-  <Voting pair={pair} />,
+ <Provider store={store}>
+    <Router history={hashHistory}>{routes}</Router>
+  </Provider>,
   document.getElementById('app')
 );
